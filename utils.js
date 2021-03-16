0e968a56417c2f1415ca2d549ee26d932ecd12e5
Akash Kumar <53407198+biharihu@users.noreply.github.com>
2021-03-16 14:59:53 +0530

    camera fullscreen resize

---
+++ utils.js
@@ -0,0 +1,51 @@
+export const generateExerciseModes = (
+  modes = [],
+  { defaultMode, before, after } = {}
+) => {
+  const initialMode = defaultMode ? defaultMode : modes[0].name
+  const ref = {
+    name: initialMode,
+    is: function (modeName) {
+      return this.name === modeName
+    },
+  }
+
+  const wrapper = document.createElement('div')
+  wrapper.style.background = '#ffffff20'
+  wrapper.style.textAlign = 'center'
+  wrapper.style.position = 'fixed'
+  wrapper.style.bottom = '0'
+  wrapper.style.width = '100%'
+  wrapper.style.padding = '5px'
+  wrapper.style.zIndex = 1
+  wrapper.style.whiteSpace = 'nowrap'
+  wrapper.style.overflowX = 'auto'
+
+  modes.forEach(mode => {
+    const button = document.createElement('button')
+
+    button.textContent = mode.name
+    button.style.margin = '5px'
+    button.style.padding = '5px'
+
+    button.addEventListener('click', event => {
+      before?.()
+
+      ref.name = mode.name
+      mode.handler(event)
+
+      modes
+        .filter(({ name }) => name !== mode.name)
+        .forEach(otherMode => otherMode.dispose?.(event))
+
+      after?.()
+    })
+    wrapper.append(button)
+
+    if (mode.name === initialMode) button.click()
+  })
+
+  document.body.prepend(wrapper)
+
+  return ref
+}
