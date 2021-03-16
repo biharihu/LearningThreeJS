2c600acf94efd119ed09453f5a918dac48f1f844
Akash Kumar <53407198+biharihu@users.noreply.github.com>
2021-03-16 17:12:08 +0530

    geomerites done

--- 09-geometries/src/script.js
+++ 09-geometries/src/script.js
@@ -16,10 +16,13 @@ const scene = new THREE.Scene();
  */
 // const geometry = new THREE.BoxBufferGeometry(1, 1, 1, 2, 2, 2);
 const geometry = new THREE.BufferGeometry();
-const positionsArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
 
+const count = 500;
+const positionsArray = new Float32Array(count * 3 * 3);
+for (let i = 0; i < count * 3 * 3; i++) {
+  positionsArray[i] = (Math.random() - 0.5) * 4;
+}
 const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
-
 geometry.setAttribute("position", positionsAttribute);
 
 const material = new THREE.MeshBasicMaterial({
