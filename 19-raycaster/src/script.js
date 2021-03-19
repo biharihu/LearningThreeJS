43c6283bc7892c29c037d1b25eb3df7f5e5fde7f
Akash Kumar <53407198+biharihu@users.noreply.github.com>
2021-03-19 15:51:00 +0530

    raycaster

--- 19-raycaster/src/script.js
+++ 19-raycaster/src/script.js
@@ -75,6 +75,36 @@ window.addEventListener("resize", () => {
 });
 
 /**
+ * Mouse
+ */
+const mouse = new THREE.Vector2();
+
+window.addEventListener("mousemove", (_event) => {
+  mouse.x = (_event.clientX / sizes.width) * 2 - 1;
+  mouse.y = -(_event.clientY / sizes.height) * 2 + 1;
+  // console.log(mouse);
+});
+
+window.addEventListener("click", () => {
+  if (currentIntersect) {
+    switch (currentIntersect.object) {
+      case object1:
+        console.log("click on object 1");
+        break;
+      case object2:
+        console.log("click on object 2");
+        break;
+      case object3:
+        console.log("click on object 3");
+        break;
+      default:
+        console.log("nothing");
+        break;
+    }
+  }
+});
+
+/**
  * Camera
  */
 // Base camera
@@ -105,6 +135,8 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  */
 const clock = new THREE.Clock();
 
+let currentIntersect = null;
+
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
@@ -114,11 +146,8 @@ const tick = () => {
   object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
 
   // Cast a ray
-  const rayOrigin = new THREE.Vector3(-3, 0, 0);
-  const rayDirection = new THREE.Vector3(1, 0, 0);
-  rayDirection.normalize();
 
-  raycaster.set(rayOrigin, rayDirection);
+  raycaster.setFromCamera(mouse, camera);
 
   const objectsToTest = [object1, object2, object3];
   const intersects = raycaster.intersectObjects(objectsToTest);
@@ -130,6 +159,35 @@ const tick = () => {
   for (const intersect of intersects) {
     intersect.object.material.color.set("#0000ff");
   }
+
+  if (intersects.length) {
+    if (currentIntersect === null) {
+      console.log("mouse enter");
+    }
+    currentIntersect = intersects[0];
+  } else {
+    if (currentIntersect) {
+      console.log("mouse leave");
+    }
+    currentIntersect = null;
+  }
+
+  // const rayOrigin = new THREE.Vector3(-3, 0, 0);
+  // const rayDirection = new THREE.Vector3(1, 0, 0);
+  // rayDirection.normalize();
+
+  // raycaster.set(rayOrigin, rayDirection);
+
+  // const objectsToTest = [object1, object2, object3];
+  // const intersects = raycaster.intersectObjects(objectsToTest);
+
+  // for (const object of objectsToTest) {
+  //   object.material.color.set("#ff0000");
+  // }
+
+  // for (const intersect of intersects) {
+  //   intersect.object.material.color.set("#0000ff");
+  // }
 
   // Update controls
   controls.update();
